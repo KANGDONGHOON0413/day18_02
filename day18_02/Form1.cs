12d24ccd452102837177ed668899ba9903ed00e9
KANGDONGHOON0413 <kmerits@gmail.com>
2022-07-01 23:31:57 +0900

    초기 커밋.

---
+++ day18_02/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace day18_02
+{
+    public partial class Form1 : Form
+    {
+        string mouseposition;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            mouseposition = "X: " + e.X + ",\tY: " + e.Y;
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawString(mouseposition, Font, Brushes.Black, 10, 10);
+        }
+    }
+}
